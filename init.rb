65fa055183ab953b0d3c3d5c41c68ebe3c35f9ff
Vincent <vincent.robert@nanego.com>
2023-05-30 13:30:26 +0200

    Init new plugin

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :redmine_coauthors do
+  name 'Redmine Coauthors plugin'
+  author 'Vincent ROBERT'
+  description 'This is a plugin for Redmine which adds coauthors to issues.'
+  version '0.0.1'
+  url 'https://github.com/nanego/redmine_coauthors'
+  author_url 'https://github.com/nanego'
+end
